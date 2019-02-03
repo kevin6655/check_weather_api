d6c62fbe1314c58abc164254701718deae4ae779
kevin6655 <daitanj65@gmail.com>
2019-02-04 00:12:07 +0900

    first_commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# api_py
+
+api をいじってみたいです。  
+天気予報が調べられます。
+
+## study する目的
+
+- api を使って軽いスクレイピングをしてみたい
+- 業務で python を使うかもしれないのでその予習
+- 今後のために勉強したくなった。
+
+## Dependency
+
+- Python 3.7.1 64bit(anaconda3-2018.12)
+
+使用したライブラリは随時更新します。
+
+- requests 2.21.0
+- json
+
+## api
+
+[OpenWetherMap](https://openweathermap.org/)
+
+## Refarences
+
+[【Python3】WebAPI を叩く【requests】](https://qiita.com/shunyooo/items/b408b8d61f9f73b21da7)
---
+++ src/api_py.py
@@ -0,0 +1,37 @@
+# 参考にさせていただいたソース
+# https://qiita.com/shunyooo/items/b408b8d61f9f73b21da7
+
+# import [ライブラリ名]でライブラリを読み込む
+import requests  # httpリクエストを飛ばせる
+import json  # json形式で取得するため使う。
+
+# WebAPIで天気を調べる。
+# apikeyを設定
+apikey = '取得したapi'
+
+# 調べたい天気の地域を配列に入れる
+areas = ['Tokyo,JP', 'Oranda,US', 'Nara,JP']
+
+# apiの雛形を格納する
+# @argument city:調べたい地域が入る
+# @argument key:apikeyが入る
+api = 'http://api.openweathermap.org/data/2.5/forecast?q={city}&APPID={key}'
+
+# 配列に格納されている数の天気の情報を得る。
+
+for cityname in areas:
+    # urlを取得する。
+    # 対象の地域を1地域づつ見ていく。
+    weather_apiurl = api.format(city=cityname, key=apikey)
+    # 確認用　print(weather_apiurl)
+    # apiにリクエストを送る。requests.get(url)で取得可能
+    url_request = requests.get(weather_apiurl)
+    # print(url_request.text)
+    # 結果はjson形式で取得になるので、データの変換を行う。json.load(対象のjsonファイル→今回だったら'url_request')
+    weather_result = json.loads(url_request.text, encoding="utf-8")
+    # 結果を出力
+    # 都市名
+    # アクセスの仕方の問題なのか大もとの["city"]を経由して順番に欲しい値を呼び出さないと値が取得できない。
+    print('地域名は', weather_result["city"]['name'])
+    print('風速は',weather_result["list"][1]['wind']['speed'])
+    print('---')
