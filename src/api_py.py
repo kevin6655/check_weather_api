d584b5ae12cc24407991af0a02dc46db119ea22e
kevin6655 <daitanj65@gmail.com>
2019-02-10 16:22:01 +0900

    処理の切り分けとresult.jsonを削除

--- src/api_py.py
+++ src/api_py.py
@@ -5,32 +5,37 @@
 import requests  # httpリクエストを飛ばせる
 import json  # json形式で取得するため使う。
 
-# WebAPIで天気を調べる。
-# apikeyを設定
-apikey = '取得したapi'
 
-# 調べたい地域を配列に入れる
-areas = ['調べたい地域名']
+def output_weather_forecast_information(CITYNAMES, APIKEY, APIURL):
+    # 配列に格納されている数の天気の情報を得る。
+    for cityname in CITYNAMES:
+        # urlを取得する。
+        # 対象の地域を1地域づつ見ていく。
+        weather_apiurl = APIURL.format(city=cityname, key=APIKEY)
+        # apiにリクエストを送る。requests.get(url)で取得可能
+        url_request = requests.get(weather_apiurl)
+        # print(url_request.text)
+        # 結果はjson形式で取得になるので、データの変換を行う。json.load(対象のjsonファイル→今回だったら'url_request')
+        weather_result = json.loads(url_request.text, encoding="utf-8")
+        # json形式でファイル出力
+        with open(cityname + '_Weather_Result.json', 'w') as result_json:
+            json.dump(weather_result, result_json,
+                      indent=2, ensure_ascii=False)
+        print('出力完了')
 
-# apiの雛形を格納する
-# @argument city:調べたい地域が入る
-# @argument key:apikeyが入る
-api = 'http://api.openweathermap.org/data/2.5/forecast?q={city}&APPID={key}'
 
-# 配列に格納されている数の天気の情報を得る。
+if __name__ == "__main__":
 
-for cityname in areas:
-    # urlを取得する。
-    # 対象の地域を1地域づつ見ていく。
-    weather_apiurl = api.format(city=cityname, key=apikey)
-    # 確認用　print(weather_apiurl)
-    # apiにリクエストを送る。requests.get(url)で取得可能
-    url_request = requests.get(weather_apiurl)
-    # print(url_request.text)
-    # 結果はjson形式で取得になるので、データの変換を行う。json.load(対象のjsonファイル→今回だったら'url_request')
-    weather_result = json.loads(url_request.text, encoding="utf-8")
-    # json形式でファイル出力
-    # 地域名ごとにファイルを出力する。
-    with open(cityname + '_Weather_Result.json', 'w') as result_json:
-        json.dump(weather_result, result_json, indent=2, ensure_ascii=False)
-    print('出力完了')
+    # WebAPIで天気を調べる。
+    # apikeyを設定
+    apikey = '取得したAPIKEY'
+
+    # 調べたい地域を配列に入れる
+    areas = ['地域名']
+
+    # apiの雛形を格納する
+    # @argument city:調べたい地域が入る
+    # @argument key:apikeyが入る
+    api = 'http://api.openweathermap.org/data/2.5/forecast?q={city}&APPID={key}'
+
+    output_weather_forecast_information(areas, apikey, api)
